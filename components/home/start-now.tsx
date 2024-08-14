f9aa045785f34e4d652c38c453f10a74eb10661e
adrgarcha <adrgarcha@alum.us.es>
2024-08-14 14:26:40 +0200

    add start now section

--- components/home/start-now.tsx
+++ components/home/start-now.tsx
@@ -1,3 +1,11 @@
+import SearchBar from '../searchbar';
+
 export default function StartNow() {
-   return <section className="border-t">StartNow</section>;
+   return (
+      <section className="flex flex-col justify-center items-center border-t h-96">
+         <h4 className="text-3xl font-bold">Start making memes now!</h4>
+         <p className="text-gray">Create memes easier and faster with Memify</p>
+         <SearchBar className={['mt-14']} redirect />
+      </section>
+   );
 }
